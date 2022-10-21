fc7a21689128928f18f6ede076fb6a0765507abf
mapuuu <phutoannguyen2271@gmail.com>
2022-10-21 15:42:34 +0700

    update todos

--- src/App.js
+++ src/App.js
@@ -1,28 +1,78 @@
 import { useState} from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   const [work, setWork] = useState('');
-  console.log(work);
+  const [todos, setTodos] = useState([]);
+  const handleAdd =() => {
 
+    if (todos?.some(item => item.id === work?.replace(/\s/g, ''))){
+      toast.warn('Công việc đã tồn tại!!!');
+    }else{
+      setTodos(prev => [...prev, { id: work?.replace(/\s/g, ''), job: work }]);
+      setWork('');
+      toast.success('Thêm công việc thành công!');
+    }
+  }
+
+  const handleDeleteJob = (id) => {
+    setTodos(prev => prev.filter(item => item.id !== id));
+    toast.success('Đã xóa thành công!')
+  }
 
   return (
-    <div className="flex gap-8 h-screen items-center justify-center">
-      <input 
-        type="text"
-        className="outline-none border border-blue-600 px-4 py-2 w-[400px]"
-        value={work}
-        onChange={ e => setWork(e.target.value)}
+    <>
+      <div className="flex flex-col gap-8 h-screen items-center justify-center">
+        <div className='flex gap-8'>
+          <input 
+            type="text"
+            className="outline-none border border-blue-600 px-4 py-2 w-[400px]"
+            value={work}
+            onChange={ e => setWork(e.target.value)}
+          />
+
+          <button
+            type="button"
+            className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white"
+            onClick={handleAdd}
+          >
+            Add
+          </button>
+        </div>
+
+        <div>
+          <h3 className='font-bold text-xl'>Context:</h3>
+
+          <ul>
+            {todos?.map((item) => {
+              return (
+                <li key={item.id} className='flex gap-10 items-center'>
+                  <span className='my-2'>{item.job}</span>
+                  <span onClick={() => handleDeleteJob(item.id)} className='my-2 cursor-pointer p-2'>X</span>
+                </li>
+              )
+            })}
+          </ul>
+
+        </div>
+      </div>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
       />
-
-      <button
-        type="button"
-        className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white"
-      >
-        Add
-      </button>
-
-    </div>
+    </>
+    
   );
 }
 
